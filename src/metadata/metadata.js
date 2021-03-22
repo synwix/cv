d3184f25c3d5d19f4647f339953dbbcf7cd2d43f
Mehmet Alp Karatepe <irakasynwix@gmail.com>
2021-03-22 23:50:14 +0300

    cv

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -1,68 +1,49 @@
 module.exports = {
-  name: 'William Shakespeare',
-  title: 'Poet, playwright and actor',
+  name: 'Mehmet Alp Karatepe',
+  title: 'Computer Engineering Student',
+  baseUrl: 'https://synwix.github.io/cv/',
   facts: {
-    'Residence': 'London',
-    'Homepage': `<a href="https://en.wikipedia.org/wiki/William_Shakespeare">
-                  <span class="print">https://en.wikipedia.org/wiki/William_Shakespeare</span>
-                  <span class="screen">wiki</span>
-                </a>`,
-    'Email': '<a href="mailto:wil64@yahoo.com">wil64@yahoo.com</a>'
+    Residence: '<a href="https://goo.gl/maps/AqFBJBhf5AQEWgAG7"><i class="fa fa-home fact-icon"></i>Ankara, Turkey</a>',
+    LinkedIn: '<a href="https://www.linkedin.com/in/mehmet-alp-karatepe-1a319b1a8/"><i class="fab fa-linkedin fact-icon"></i> Mehmet Alp Karatepe</a>',
+    GitHub: '<a href="https://github.com/synwix"><i class="fab fa-github fact-icon"></i> synwix</a>',
+    StackOverflow: '<a href="https://stackoverflow.com/users/15454125/synwix"><i class="fab fa-stack-overflow fact-icon"></i> synwix</a>',
+    HackerRank: '<a href="https://www.hackerrank.com/synwix"><i class="fab fa-hackerrank fact-icon"></i> synwix</a>',
+    Email: '<a href="mailto:m.alp.krtp@gmail.com">m.alp.krtp@gmail.com</a>',
   },
   skills: [
-    ['Comedy', '++++'],
-    ['Love', '++++'],
-    ['Theater', '++++'],
-    ['Tragedy', '++++'],
-    ['Poetry', '++++'],
-    ['Erotic', '+++'],
-    ['Sonnets', '++++'],
-    ['Plays', '++++'],
-    ['Writing', '++++'],
-    ['Directing', '+++'],
-    ['Literature', '++++'],
-    ['Influence', '++++'],
+    ['Java', '++++'],
+    ['C#', '+++'],
+    ['C/C++', '+++'],
+    ['Python', '++++'],
+    ['JavaScript', '++++'],
+    ['Node JS', '+++'],
+    ['Firebase', '+++'],
+    ['Matlab', '++'],
+    ['React JS', '+++'],
+    ['Django', '+++'],
+    ['Git', '+++'],
+    ['HTML', '+++'],
+    ['CSS', '+++'],
+    ['Keras', '++++'],
+    ['Tensorflow', '++++'],
+    ['.NET', '+++'],
+    ['npm', '+++'],
   ],
   plays: [
     {
-      title: 'Hamlet',
-      written: 'Likely early 17th century',
-      badges: ['tragedy', 'philosophy', 'death'],
+      title: 'Chat-App',
+      written: 'Full-Stack Web App',
+      badges: ['JavaScript', 'React JS', 'HTML', 'CSS', 'Chat Engine API'],
       contents: `
-Prince Hamlet is visited by his father's ghost and ordered to avenge his father's murder by killing King Claudius, his uncle. After struggling with several questions, including whether what the ghost said is true and whether it is right for him to take revenge, Hamlet, along with almost all the other major characters, is killed.
-`
+The Web Application that I developed with the use of JavaScript, React, HTML, CSS in Front-End and with the usage of Chat Engine as the Back-End to store messages in real time, at which I used JS to communicate with the API`
     },
     {
-      title: 'Twelfth Night',
-      written: '1600 – 1601',
-      badges: ['comedy', 'love'],
+      title: 'Teknofest Machine Learning Competition',
+      written: 'Machine Learning Competition',
+      badges: ['Keras', 'Tensorflow', 'Machine Learning Research'],
       contents: `
-Viola finds herself shipwrecked in Illyria and, assuming that her brother Sebastian has died in the wreck, disguises herself as a man to gain a position in Duke Orsino's court. Orsino sends Viola (whom he knows as Cesario) to deliver a message to his love, Olivia. Olivia, however, dislikes the Duke. She falls in love with Viola, who she thinks is a man. Eventually, Viola's brother Sebastian, who in fact was unharmed in the wreck, reappears. At a critical moment, Viola's true identity is revealed when members of the court notice the similarities between her and Sebastian. Olivia quickly falls in love with Sebastian, and Viola confesses her love for the Duke.
+I'm currently participating in a Machine Learning competition project as a group in Teknofest, which involves object detection we use Keras to develop a Machine Learning model for.
 `
     },
-    {
-      title: 'The Tragedy of Romeo and Juliet',
-      written: '1595–1596, with a possible early draft written in 1591',
-      badges: ['tragedy', 'death', 'love'],
-      contents: `
-In Verona, Italy, two families, the Montagues and the Capulets, are in the midst of a bloody feud. Romeo, a Montague, and Juliet, a Capulet, fall in love and struggle to maintain their relationship in the face of familial hatred. After Romeo kills Juliet's cousin Tybalt in a fit of passion, things fall apart. Both lovers eventually commit suicide within minutes of each other, and the feuding families make peace over their recent grief.
-`
-    }
-  ],
-  experience: [
-    {
-      header: 'Performance',
-      skills: ['theater', 'play'],
-      contents: `
-Thousands of performances of my plays have been staged since the end of the 16th century. Many of my greatest plays were performed by the Lord Chamberlain's Men and King's Men acting companies at the [Globe](https://en.wikipedia.org/wiki/Globe_Theatre) and Blackfriars Theatres.
-`
-    },
-    {
-      header: 'Poetry',
-      skills: ['poetry', 'erotic'],
-      contents: `
-In 1593 and 1594, when the theatres were closed because of plague, I published two narrative poems on sexual themes, Venus and Adonis and The Rape of Lucrece. He dedicated them to Henry Wriothesley, Earl of Southampton.
-`
-    }
   ]
 };
